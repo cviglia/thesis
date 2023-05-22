42a20b9a01dbfb03406f81033c166cf3bf5a9b92
cviglia <92572568+cviglia@users.noreply.github.com>
2023-05-22 12:00:58 +0200

    Update data.r

--- data.r
+++ data.r
@@ -4,3 +4,30 @@ setwd("C:/Rtesi/")
 # see the old data
 life4p <- read.csv(file = "data/3792.csv", header = TRUE, sep = ",")
 head(life4p)
+
+...............................................................
+setwd("C:/Rtesi/")
+
+install.packages('bipartite')
+library(bipartite) 
+
+data("memmott1999") 
+View(memmott1999) 
+plotweb(memmott1999) 
+visweb(memmott1999) 
+
+
+
+................................................................
+setwd("C:/Rtesi/")
+
+library(vegan)
+
+sub <- read.csv("data/submission_en.node.3792.csv", 
+         header = TRUE, sep = ",")
+View(sub)
+
+
+sub_env <- read.table("data/sub.txt",
+           header = TRUE, sep = ",")
+View(sub_env)
