b04ac423de96a31c7297baf41facbf430a377bec
cviglia <92572568+cviglia@users.noreply.github.com>
2023-05-30 11:22:53 +0200

    Update data.r

--- data.r
+++ data.r
@@ -1,11 +1,3 @@
-# set directory
-setwd("C:/Rtesi/")
-
-# see the old data
-life4p <- read.csv(file = "data/3792.csv", header = TRUE, sep = ",")
-head(life4p)
-
-#...............................................................
 setwd("C:/Rtesi/")
 
 library(bipartite) 
