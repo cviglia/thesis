9e70f4013775fe303a6246c3ef6d3c9bae7a7ce3
cviglia <92572568+cviglia@users.noreply.github.com>
2023-06-06 16:08:10 +0200

    Update data.r

--- data.r
+++ data.r
@@ -25,3 +25,7 @@ sub_env <- read.table("data/sub_en_30_05.txt",
 View(sub_env)
 
 str(sub) #controlla struttura
+summary.data.frame(sub)
+
+barplot(table(sub$real.taxonomy.pollinator..Title)) #barplot con etichette perpendicolari
+par(las=2)
