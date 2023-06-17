fad9d9604f34d01454f89fe90a440c96a2392fae
cviglia <92572568+cviglia@users.noreply.github.com>
2023-06-17 16:12:51 +0200

    Update and rename da.r to data.r

---
+++ data.r
@@ -0,0 +1,49 @@
+# LIFE 4 P
+
+setwd("C:/Rtesi/")
+
+library(vegan)
+
+subcsv <- read.csv("data/sub_en.node.3792_30_05_Copia.csv",
+                   sep =";",
+                   header = T)
+
+View(subcsv)
+str(subcsv)
+# data.frame:	1632 obs. of  55 variables
+
+options(max.print=1000000)
+
+subcsv$real.taxonomy.pollinator..Family
+
+subcsv <- subcsv[!(subcsv$real.taxonomy.pollinator..Family == ""), ]
+
+subcsv$real.taxonomy.pollinator..Family
+str(subcsv$real.taxonomy.pollinator..Family)
+
+par(las=2)
+barplot(table(subcsv$real.taxonomy.pollinator..Family),
+        main = "Taxonomy of pollinators",
+        cex.axis=0.3, cex.names=0.3)
+
+
+png(filename = "outputs/barplot.png",
+    width = 1800,
+    height = 1800,
+    res = 300)
+par(las=2)
+barplot(table(subcsv$real.taxonomy.pollinator..Family),
+        main = "Taxonomy of pollinators",
+        cex.axis=0.3, cex.names=0.3)
+dev.off()
+
+
+str(subcsv$real.taxonomy.pollinator..Family)
+
+
+
+
+
+
+
+
