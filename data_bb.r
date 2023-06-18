b6ce9838323da79d8e2bcfea34d21c554ad7ebf9
cviglia <92572568+cviglia@users.noreply.github.com>
2023-06-18 16:14:10 +0200

    Update data_bb.r

--- data_bb.r
+++ data_bb.r
@@ -8,3 +8,37 @@ library(bipartite)
 bb <- read.csv("data/bioblitz_20.05.csv",
                sep =";", 
                header = T)
+
+# pollinators groups bioblitz
+par(las=2)
+barplot(table(bb$ï..pollinator),
+        main = "Groups of pollinators",
+        cex.axis=0.3, cex.names=0.3)
+
+# immagine png
+png(filename = "outputs/groups_bioblitz.png",
+    width = 1800,
+    height = 1800,
+    res = 300)
+par(las=2)
+barplot(table(bb$ï..pollinator),
+        main = "Groups of pollinators",
+        cex.axis=0.3, cex.names=0.3)
+dev.off()
+
+# plants groups bioblitz
+par(las=2)
+barplot(table(bb$plant),
+        main = "Families of plants",
+        cex.axis=0.3, cex.names=0.3)
+
+# immagine png
+png(filename = "outputs/families_bioblitz.png",
+    width = 1800,
+    height = 1800,
+    res = 300)
+par(las=2)
+barplot(table(bb$plant),
+        main = "Families of plants",
+        cex.axis=0.3, cex.names=0.3)
+dev.off()
