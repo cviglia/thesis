308227d3a7eb63151b4622a9aa89554a51d05e4e
cviglia <92572568+cviglia@users.noreply.github.com>
2023-07-04 18:06:56 +0200

    Update data_bb.r

--- data_bb.r
+++ data_bb.r
@@ -5,40 +5,43 @@ setwd("C:/Rtesi/")
 library(vegan)
 library(networkD3)
 
-bb <- read.csv("data/bioblitz_20.05.csv",
-               sep =";", 
-               header = T)
+bb20 <- read.csv("data/bioblitz_20.05.csv",
+                 sep =";", 
+                 header = T)
+bb20
+View(bb20)
 
-# pollinators groups bioblitz
+# pollinators groups bioblitz 20
 par(las=2)
-barplot(table(bb$ï..pollinator),
+barplot(table(bb20$ï..pollinator),
         main = "Groups of pollinators",
         cex.axis=0.3, cex.names=0.3)
 
 # immagine png
-png(filename = "outputs/groups_bioblitz.png",
+png(filename = "outputs/groups_bioblitz_20.png",
     width = 1800,
     height = 1800,
     res = 300)
 par(las=2)
-barplot(table(bb$ï..pollinator),
+barplot(table(bb20$ï..pollinator),
         main = "Groups of pollinators",
         cex.axis=0.3, cex.names=0.3)
 dev.off()
 
-# plants groups bioblitz
+
+# plants families bioblitz
 par(las=2)
-barplot(table(bb$plant),
+barplot(table(bb20$plant),
         main = "Families of plants",
         cex.axis=0.3, cex.names=0.3)
 
 # immagine png
-png(filename = "outputs/families_bioblitz.png",
+png(filename = "outputs/families_bioblitz_20.png",
     width = 1800,
     height = 1800,
     res = 300)
 par(las=2)
-barplot(table(bb$plant),
+barplot(table(bb20$plant),
         main = "Families of plants",
         cex.axis=0.3, cex.names=0.3)
 dev.off()
@@ -49,11 +52,59 @@ setwd("C:/Rtesi/")
 library(bipartite)
 library(openxlsx)
 
-network_bb <- read.xlsx("data/bb20.05_network.xlsx", colNames = TRUE, rowNames = TRUE)
+network_bb20 <- read.xlsx("data/bb20.05_network.xlsx", colNames = TRUE, rowNames = TRUE)
 
-plotweb(network_bb, text.rot=90)
+plotweb(network_bb20, text.rot=90)
+
+
 
 # bioblitz 6 maggio
+bb06 <- read.csv("data/bioblitz_06.05.csv",
+               sep =";", 
+               header = T)
 
+# pollinators groups bioblitz
+par(las=2)
+barplot(table(bb06$ï..pollinator),
+        main = "Groups of pollinators",
+        cex.axis=0.3, cex.names=0.3)
+
+# immagine png
+png(filename = "outputs/groups_bioblitz_06.png",
+    width = 1800,
+    height = 1800,
+    res = 300)
+par(las=2)
+barplot(table(bb06$ï..pollinator),
+        main = "Groups of pollinators",
+        cex.axis=0.3, cex.names=0.3)
+dev.off()
+
+# plants groups bioblitz
+par(las=2)
+barplot(table(bb06$plant),
+        main = "Families of plants",
+        cex.axis=0.3, cex.names=0.3)
+
+# immagine png
+png(filename = "outputs/families_bioblitz_06.png",
+    width = 1800,
+    height = 1800,
+    res = 300)
+par(las=2)
+barplot(table(bb06$plant),
+        main = "Families of plants",
+        cex.axis=0.3, cex.names=0.3)
+dev.off()
+
+
+# network
+setwd("C:/Rtesi/")
+library(bipartite)
+library(openxlsx)
+
+network_bb06 <- read.xlsx("data/bb06.05_network.xlsx", colNames = TRUE, rowNames = TRUE)
+
+plotweb(network_bb06, text.rot=90)
 
 
