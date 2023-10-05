b9ff7d8fdf8a023a0dbc49b63b4b0598bb47e863
cviglia <92572568+cviglia@users.noreply.github.com>
2023-10-05 13:00:01 +0200

    Create packages.r

---
+++ packages.r
@@ -0,0 +1,25 @@
+# pacchetti
+
+library(vegan)
+# ???
+
+library(openxlsx)
+# to read the worksheet
+
+library(bipartite)
+# to make the network
+
+library(ggplot2)
+# for the plot, graph
+
+library(hrbrthemes)
+# for the theme with colours
+
+library(MASS)
+# correlazione, regression analysis
+
+library(networkD3)
+# sankey diagram
+
+library(dplyr)
+# manipulation of sankey
